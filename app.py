9f7ca7e3e5b33aee396cc60232712cafdfc553bc
joseantoniocali007-cyber <joseantoniocali007@gmail.com>
2026-05-13 11:59:06 +0200

    Proyecto listo

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+import psycopg2
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+DB_URL = os.environ.get('DATABASE_URL')
+
+def get_db_connection():
+    return psycopg2.connect(DB_URL)
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT id, nombre, email FROM usuarios ORDER BY id;')
+    usuarios = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', usuarios=usuarios)
+
+@app.route('/add', methods=['POST'])
+def add_user():
+    nombre, email = request.form['nombre'], request.form['email']
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO usuarios (nombre, email) VALUES (%s, %s)', (nombre, email))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM usuarios WHERE id = %s', (id,))
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
